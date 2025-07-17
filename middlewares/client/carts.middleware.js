665fa117d97f190ec6aded5df8368ef475aab9a7
LTN130925 <ltansang374@gmail.com>
2025-07-17 17:15:09 +0700

    cập nhật lại phần middleware cart

--- middlewares/client/carts.middleware.js
+++ middlewares/client/carts.middleware.js
@@ -1,37 +1,28 @@
 const Cart = require('../../models/cart.model');
 
 module.exports.cartId = async (req, res, next) => {
+  const cart = await Cart.findOne({
+    _id: req.cookies.cart_id,
+  });
   try {
-    let cart = null;
-    if (req.cookies.cart_id) {
-      cart = await Cart.findOne({ _id: req.cookies.cart_id });
-    }
-
     if (!cart) {
-      if (req.cookies.cart_created_recently) {
-        return res.status(429).send('Bạn vừa tạo giỏ hàng, vui lòng chờ một lát rồi thử lại!');
-      }
-      cart = new Cart();
-      await cart.save();
-      res.cookie('cart_id', cart.id, {
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-        httpOnly: true,
-      });
+      const newCart = new Cart();
+      await newCart.save();
 
-      res.cookie('cart_created_recently', '1', {
-        maxAge: 30 * 1000,
-        httpOnly: true,
+      // set time expiresAt cart
+      const expiresAt = 1000 * 60 * 60 * 24 * 30;
+      res.cookie('cart_id', newCart.id, {
+        expires: new Date(Date.now() + expiresAt),
       });
     }
-
-    const totalQuality = cart.products.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
-    res.locals.totalQuality = totalQuality;
-  } catch (error) {
-    console.error('Error in cart middleware:', error.message);
-    res.locals.totalQuality = 0;
+    if (cart.products.length > 0) {
+      res.locals.totalQuality = cart.products.reduce(
+        (total, item) => total + item.quantity,
+        0
+      );
+    }
+  } catch (err) {
+    console.log('error create new cart ' + err.message);
   }
 
   next();
